a547bb6a7f4dca9f1dbd2c114681c2e5a2df5478
naira-maximo <naira.maximo@hotmail.com>
2024-08-14 16:55:43 -0300

    NM: definição de arrays de nomes e naipes; contrutor Baralho para inicializar as cartas

--- Baralho.java
+++ Baralho.java
@@ -1,38 +1,61 @@
+// Crie uma classe Baralho, que possui 56 cartas. No construtor de Baralho, inicialize as 56 cartas (juntamente com os 4 coringas, que devem se chamar “coringa” e o naipe é “coringa”).
 public class Baralho {
-    private String[] nomes = new String[13];
-    private String[] naipes = new String[5];
-
-    private String[] baralho = new String[56];
+    private String[] naipes = {"ouros", "copas", "espadas", "paus"};
+    private String[] nomes = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+    private Carta[] baralho = new Carta[56];
     
-    private String nome;
-    private String naipe;
-    Carta cards = new Carta(nome, naipe);
+    // César, no enunciado, a inicialização das cartas deve ser feita no contrutor. Usei o exemplo de arrays com os índices de nomes e naipes. 
+    public Baralho() {
+        int index = 0;
 
-    public String[] criarCarta(String nome, String naipe) {
-        Carta carta = new Carta(nome, naipe);
-        return baralho;
+        for (String naipeCarta : naipes) {
+            for (String nomeCarta : nomes) {
+                baralho[index++] = new Carta(nomeCarta, naipeCarta);
+            }
+        }
+        // César, deixei a criação dos coringas separado e o nome e naipe fora das arrays. O que acha?
+        for (int i = 0; i < 4; i++) {
+            baralho[index++] = new Carta("coringa", "coringa");
+        }
     }
-    public String[] embaralhar(){
+
+    // César, deixei comentado o nosso método de construção de cartas.
+    // public Carta[] criarCarta() {
+    //     int index = 0;
+    //     for (String naipeCarta : naipes) {
+    //         for (String nomeCarta : nomes) {
+    //             baralho[index++] = new Carta(nomeCarta, naipeCarta);
+    //         }
+    //     }
+    //     for (int i = 0; i < 4; i++) {
+    //         baralho[index++] = new Carta("coringa", "coringa");
+    //     }
+    //     return baralho;
+    // }
+
+    // O método usa o math.random para trocar as cartas dentro do baralho, misturando-as (sorteie duas posições e troque-as. Repita este processo diversas vezes). 
+    public Carta[] embaralhar(){
         //Math.random
         return baralho;
     }
 
+    // O método devolve uma carta, retirada do topo do baralho, se não estiver vazio, ou null.
     public String distribuirCarta() {
         String carta = null;
         return carta;
     }
 
+    // O método verifica se tem determinada carta no baralho, devolvendo true ou false.
     public boolean hasCarta(String nome, String naipe) {
-       if () {
-           return true;
-       } else {return false;}
+        for (Carta carta : baralho) {
+            if (carta nome == nome && carta naipe == naipe) {
+                return true;
+            } else {return false;}
+        }
     }
 
+    // Esse método imprime as cartas para verificar como estão dispostas (se estão embaralhadas por exemplo).
     public void imprimirBaralho() {
         // System.out.println();
     }
-
-
-
-
 }
--- Carta.java
+++ Carta.java
@@ -1,15 +1,21 @@
+// Crie a classe Carta, que possui um nome e um naipe. Crie os métodos GET e SET para os atributos
 public class Carta {
-    // Atributos
     private String nome;
     private String naipe;
-    // Construtor
-    public Carta (String nome, String naipe) {
+
+    public Carta(String nome, String naipe) {
         this.nome = nome;
         this.naipe = naipe;
     }
-    // Métodos de acesso
+
     public String getNome() {return nome;}
     public void setNome(String nome) {this.nome = nome;}
     public String getNaipe() {return naipe;}
     public void setNaipe(String naipe) {this.naipe = naipe;}
+
+    // César, sei o método toString para imprimir as cartas na array
+    @Override
+    public String toString() {
+        return nome + " de " + naipe;
+    }
 }
